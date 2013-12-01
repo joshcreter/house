f81aa830320b18f6b2dd00c449b7a390d7ade24a
Josh Creter <jcreter@gmail.com>
2013-12-01 15:37:08 -0800

    Converted /listen to adaptive speech

--- app/src/modules/pages/listen/ListenCtrl.js
+++ app/src/modules/pages/listen/ListenCtrl.js
@@ -1,14 +1,34 @@
-/**
-*/
+(function () {
+    'use strict';
 
-'use strict';
+    angular.module('myApp').controller('ListenCtrl', function TodoCtrl($scope, $location, todoStorage, filterFilter, $speechRecognition, appLighting) {
+        var LANG = 'en-US';
+        $speechRecognition.onstart(function(e){
+            $speechRecognition.speak('Yes?');
+        });
+        $speechRecognition.onerror(function(e){
+            var error = (e.error || '');
+            alert('An error occurred ' + error);
+        });
+        $speechRecognition.payAttention();
+        $speechRecognition.setLang(LANG);
+        $speechRecognition.listen();
 
-angular.module('myApp').controller('ListenCtrl', ['$scope',
-function($scope) {
-    $scope.speech = {
-        maxResults: 25,
-        continuous: true,
-        interimResults: true,
-        value: ''
-    };
-}]);
+        $scope.recognition = {};
+        $scope.recognition['en-US'] = {
+            'command': {
+                'regex': /^computer .+/gi,
+                'lang': 'en-US',
+                'call': function(utterance){
+
+                    appLighting.locateBridges();
+                    console.log(utterance);
+                    $speechRecognition.speak('OK');
+                }
+            }
+        };
+
+        var ignoreUtterance = {};
+        ignoreUtterance['command'] = $speechRecognition.listenUtterance($scope.recognition['en-US']['command']);
+    });
+}());
