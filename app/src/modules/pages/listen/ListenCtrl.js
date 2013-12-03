7ea830d9a3e1153e986021fae439c1b3a2e7063e
Josh Creter <jcreter@gmail.com>
2013-12-02 20:37:43 -0800

    Fixing listenCtrl

--- app/src/modules/pages/listen/ListenCtrl.js
+++ app/src/modules/pages/listen/ListenCtrl.js
@@ -1,18 +1,13 @@
 (function () {
     'use strict';
-    angular.module('myApp').controller('ListenCtrl', [$scope, $speechRecognition, function($scope, $speechRecognition) {
+    angular.module('myApp').controller('ListenCtrl', ['$scope', '$speechRecognition', 'appLighting', function ($scope, $speechRecognition, appLighting) {
         var LANG = 'en-US';
 
-        var retVal =(appLighting.test('my test val'));
-        console.log(retVal);
-        appLighting.allLightsOn();
-        appLighting.allLightsOff();
-
-        $speechRecognition.onstart(function(e){
+        $speechRecognition.onstart(function (e) {
             $speechRecognition.speak('Yes?');
         });
 
-        $speechRecognition.onerror(function(e){
+        $speechRecognition.onerror(function (e) {
             var error = (e.error || '');
             alert('An error occurred ' + error);
         });
@@ -25,16 +20,39 @@
             'command': {
                 'regex': /^computer .+/gi,
                 'lang': 'en-US',
-                'call': function(utterance){
+                'call': function (utterance) {
 
                     console.log(utterance);
                     $speechRecognition.speak('OK');
 
+                }
+            },
+            'on': {
+                'regex': /^lights on/gi,
+                'lang': 'en-US',
+                'call': function (utterance) {
+
+                    console.log(utterance);
+                    appLighting.allLightsOn();
+                    $speechRecognition.speak('OK');
+
+                }
+            },
+            'off': {
+                'regex': /^lights off/gi,
+                'lang': 'en-US',
+                'call': function (utterance) {
+
+                    console.log(utterance);
+                    appLighting.allLightsOff();
+                    $speechRecognition.speak('OK');
                 }
             }
         };
 
         var ignoreUtterance = {};
         ignoreUtterance['command'] = $speechRecognition.listenUtterance($scope.recognition['en-US']['command']);
+        ignoreUtterance['off'] = $speechRecognition.listenUtterance($scope.recognition['en-US']['off']);
+        ignoreUtterance['on'] = $speechRecognition.listenUtterance($scope.recognition['en-US']['on']);
     }]);
 });
